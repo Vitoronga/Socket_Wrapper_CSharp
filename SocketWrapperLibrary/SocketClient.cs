9a3f66771202214b70dd4669899eea6d9b10bf83
Vitoronga <vitorportella29@gmail.com>
2023-01-01 23:51:21 -0300

    Created classes SocketClient and SocketServer

--- SocketWrapperLibrary/SocketClient.cs
+++ SocketWrapperLibrary/SocketClient.cs
@@ -1,7 +1,74 @@
-﻿namespace SocketWrapperLibrary
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SocketWrapperLibrary
 {
     public class SocketClient
     {
+        protected Socket socket;
 
+        public bool Connect(string ip, int port)
+        {
+            if (socket != null) return false;
+
+            try
+            {
+                socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ip, port);
+
+            } catch (Exception e)
+            {
+                Console.WriteLine("ERROR (Connect): " + e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool SendData(byte[] bytes)
+        {
+            if (socket == null) return false;
+
+            try
+            {
+                socket.Send(bytes);
+            } catch (Exception e)
+            {
+                Console.WriteLine("ERROR (SendData): " + e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public byte[] ReceiveData()
+        {
+            byte[] formattedBytes = new byte[0];
+
+            if (socket == null) return formattedBytes;
+
+            try
+            {
+                byte[] receivedBytes = new byte[socket.ReceiveBufferSize];
+                int byteAmount = socket.Receive(receivedBytes);
+                formattedBytes = new byte[byteAmount];
+
+                for (int i = 0; i < formattedBytes.Length; i++)
+                {
+                    formattedBytes[i] = receivedBytes[i];
+                }
+
+            } catch (Exception e)
+            {
+                Console.WriteLine("ERROR (ReceiveData): " + e.Message);
+            }
+
+            return formattedBytes;
+        }
+
+        public void CloseConnection()
+        {
+            socket.Close();
+        }
     }
 }
---
+++ SocketWrapperLibrary/SocketServer.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SocketWrapperLibrary
+{
+    class SocketServer
+    {
+        private static Socket serverSocket;
+        internal static List<ClientHandler> socketConnections= new List<ClientHandler>();
+
+        public static void Start(int port)
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Loopback, port);
+
+            serverSocket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(ipEndPoint);
+            
+            while (serverSocket != null && serverSocket.IsBound)
+            {
+                serverSocket.Listen(0);
+                socketConnections.Add(new ClientHandler(serverSocket.Accept()));
+            }
+        }
+    }
+
+    internal class ClientHandler : SocketClient // Create a "SocketElements" class to provide the send and receive methods? (maybe not, but this inheritance is not good either)
+    {
+        Thread dataReceiverThread;
+
+        public ClientHandler(Socket socket)
+        {
+            this.socket = socket;
+        }
+
+        
+
+    }
+}
